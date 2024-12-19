f947769453490192b7ccbf22fa3c8659f115613a
rotemba3 <160619843+rotemba3@users.noreply.github.com>
2024-12-19 18:04:36 +0200

    19.12 work

--- ELibrary/App_Start/RouteConfig.cs
+++ ELibrary/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ELibrary
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MainLibrary", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ELibrary/Controllers/MainLibraryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELibrary.Controllers
+{
+    public class MainLibraryController : Controller
+    {
+        private readonly string ConnectionString = ConfigurationManager.ConnectionStrings["deafultConnectionString_R"].ConnectionString;
+
+        // GET: MainLibrary
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult InfoBook() 
+        {
+            return View(); 
+        }
+    }
+}
